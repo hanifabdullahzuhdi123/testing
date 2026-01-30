243b1fc6f778b7478615c065e9d3b8b97096444f
hanifabdullahzuhdi123 <ketirjoko4@gmail.com>
2026-01-30 17:01:39 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hanifabdullahzuhdi123/testing/main/Loader.lua"))()
+-- Loader.lua
+-- Memuat script utama dari HanHu.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hanifabdullahzuhdi123/testing/main/HanHub.lua"))()
